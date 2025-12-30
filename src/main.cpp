8de613974bfcf2ab22007243c9ed642242636f40
Brian Wang <bzwang@wpi.edu>
2025-12-30 12:30:16 -0500

    refactor(core): update main to accommodate file I/O

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
-#include <string>
+#include <fstream>
 #include "matching_engine.hpp"
 #include "printer_listener.hpp"
 #include "parser.hpp"
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::cerr;
+using std::ifstream;
+using std::istream;
+using std::ofstream;
+using std::ostream;
+using std::getline;
+using std::string;
 
-
-int main(){
-    MatchingEngine engine;
-    PrinterListener printer;
-    engine.add_listener(&printer);
-
-    std::string input;
-    do {
-        std::getline(cin, input);
-        auto cmd = parse_command(input);  
+void process_commands(istream& input, ostream& output, MatchingEngine& engine, PrinterListener& printer) {
+    string line;
+    while (getline(input, line)) {
+        if (line == "X") break;
+        
+        auto cmd = parse_command(line);
         switch (cmd.type) {
             case CommandType::New:
                 engine.process_new_order(cmd.order_id, cmd.side, cmd.price, cmd.qty);
@@ -35,11 +39,32 @@ int main(){
                 engine.print_book();
                 break;
             case CommandType::Exit:
-                break;
+                return;
             default:
                 break;
-        } 
-        cout << "\n"; 
-    } while (input != "X");
-    return 0;    
+        }
+        output << "\n";
+    }
+}
+
+int main(int argc, char* argv[]){
+    MatchingEngine engine;
+    PrinterListener printer;
+    engine.add_listener(&printer);
+
+    if (argc > 1){
+        ifstream input_file(argv[1]);
+        if (!input_file.is_open()){
+            cerr << "Could not open input file " << argv[1] << endl;
+            return 1;
+        }
+        // process commands from file
+        process_commands(input_file, cout, engine, printer);
+        input_file.close();
+    }
+    else {
+        // read line by line from stdin
+        process_commands(cin, cout, engine, printer);
+    }
+    return 0;  
 }
